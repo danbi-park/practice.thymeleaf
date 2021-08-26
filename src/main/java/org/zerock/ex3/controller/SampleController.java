5c333aa98197b60dc9cd2ff861b08de63ef5b433
sweg-db <bhd03067@naver.com>
2021-08-26 18:39:05 +0900

    Thymeleaf의 Layout

---
+++ src/main/java/org/zerock/ex3/controller/SampleController.java
@@ -0,0 +1,68 @@
+package org.zerock.ex3.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.ex3.dto.SampleDTO;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Controller
+@RequestMapping("/sample") //주소를 요청할 때
+@Log4j2 //log에 대한 기록 남길 때 도움
+public class SampleController {
+        @GetMapping("/ex1")
+        public void ex1(){
+            log.info("ex1..........");
+        }
+        @GetMapping({"/ex2", "/exLink"}) // {"",""} 복수개로 링크를 쓸 수 있다.
+        public void exModel(Model model) {  //model -> view에 데이터를 전달할 수 있는 객체
+            List<SampleDTO> list = IntStream.rangeClosed(1,20).asLongStream().mapToObj(i->{
+                SampleDTO dto = SampleDTO.builder()
+                        .sno(i)
+                        .first("First.." + i)
+                        .last("Last.." + i)
+                        .regTime(LocalDateTime.now())
+                        .build();
+                return dto;
+            }).collect(Collectors.toList());
+            model.addAttribute("list",list);
+        } //복수개가 넘어옴 예)게시판에 게시 글번호, 작성자, 작성년월일이 한 행인데 그게 하나의 sampledto라고 생각하면 됨
+
+    @GetMapping("/exInline")
+    public String exInline(RedirectAttributes redirectAttributes){
+        log.info("exInline....");
+        SampleDTO dto = SampleDTO.builder()
+                .sno(100L)
+                .first("First..100")
+                .last("Last..100")
+                .regTime(LocalDateTime.now())
+                .build();
+        redirectAttributes.addFlashAttribute("result","success");
+        redirectAttributes.addFlashAttribute("dto",dto);
+        return "redirect:/sample/ex3";
+    }
+
+    @GetMapping("/ex3")
+    //void는 물어본 url그대로 resource, ex3라는 것이 있어야 함
+    public void ex3(){
+        log.info("ex3...");
+    }
+
+    @GetMapping({"/exLayout1","/exLayout2"})
+    public void exLayout(){
+        log.info("exLayout...");
+    }
+
+    @GetMapping("/exTemplate")
+    public void exTemplate(){
+        log.info("exTemplate...");
+    }
+
+}
